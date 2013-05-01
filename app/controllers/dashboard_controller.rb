483c9ab410c83a788eecc9e5170e93b7d87c5af8
Cam Tyler <codectc@gmail.com>
2013-05-01 19:38:32 -0400

    Ability to see everyone's tags.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,18 @@ class DashboardController < ApplicationController
     @marks = current_user.marks.order("created_at DESC")
   end
 
-  def sort_by_tag
+  def show_user_tags
     @tag = params[:tag]
     @marks = current_user.marks.tagged_with(@tag, :owned_by => current_user)
+    render 'index'
+  end
+
+  def show_all_tags
+    @tag = params[:tag]
+    tag_id = Tag.find_by_name(@tag)
+    @marks = Mark.find(:all,
+                :joins => [:taggings],
+                :conditions => ['taggings.tagger_id IS NOT NULL AND taggings.tag_id = ?', tag_id])
     render 'index'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Zipmark::Application.routes.draw do
 
   root :to => 'dashboard#index'
 
-  match '/tags/:tag' => 'dashboard#sort_by_tag', as: :tag
+  match 'dashboard/tags/:tag' => 'dashboard#show_user_tags', as: :tag
+  match 'tags/:tag' => 'dashboard#show_all_tags', as: :all_tag
+  
   match '/add_mark/?url=:url&title=:title' => 'dashboard#add_mark', as: :add_mark
 end
